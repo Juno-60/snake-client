ad81d7378207136abfd7eb3e65e96e4d82eda3e3
Nick Rusin <woundculture@gmail.com>
2021-12-01 20:30:13 +0000

    added name and connection succesful message

--- client.js
+++ client.js
@@ -1,18 +1,24 @@
 const net = require('net');
 
-// establishes a connection with the server
 const connect = function() {
+  // establishes a connection with the server
   const conn = net.createConnection({
     host: '165.227.47.243',
     port: 50541,
+  })  
+  // return success message if connected
+  conn.on("connect", () => {
+    console.log("Successfully connected!");
+  });
+  conn.on("connect", () => {
+    conn.write("Name: N.R");
   })
   // interpret incoming data as text:
   conn.setEncoding('utf8');
-  // return conn;
   conn.on("data", (data) => {
     console.log(data);
-    return conn;
-  })
+  return conn;
+  })  
 };
 
 module.exports = { 
